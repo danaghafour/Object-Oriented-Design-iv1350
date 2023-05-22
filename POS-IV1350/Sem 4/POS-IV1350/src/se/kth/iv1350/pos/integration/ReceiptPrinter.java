0e8631eae89c9bd3f78f89512c82d6e1899e7084
danaghafour <118491580+danaghafour@users.noreply.github.com>
2023-05-22 23:43:15 +0200

    Add files via upload

---
+++ POS-IV1350/Sem 4/POS-IV1350/src/se/kth/iv1350/pos/integration/ReceiptPrinter.java
@@ -0,0 +1,12 @@
+package se.kth.iv1350.pos.integration;
+
+/**
+ * This class represents a receipt printer used for printing receipts during a sale.
+ */
+public class ReceiptPrinter {
+    /**
+     * Creates a new instance of the ReceiptPrinter class.
+     */
+    public ReceiptPrinter(){
+    }
+}
