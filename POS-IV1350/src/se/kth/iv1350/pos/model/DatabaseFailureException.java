4bf14aad9841c4befbcc498ee09a43b5e4a18e12
danaghafour <118491580+danaghafour@users.noreply.github.com>
2024-03-19 11:17:56 +0100

    Delete POS-IV1350 directory

--- POS-IV1350/src/se/kth/iv1350/pos/model/DatabaseFailureException.java
+++
@@ -1,8 +0,0 @@
-package se.kth.iv1350.pos.model;
-
-// A checked exception class that indicates a database failure
-public class DatabaseFailureException extends Exception {
-    public DatabaseFailureException(String message) {
-        super(message);
-    }
-}
